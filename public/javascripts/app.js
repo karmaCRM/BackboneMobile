9ca7353b3843532c8b11b926ebe40180da737e47
John Paul Narowski <jnarowski@gmail.com>
2012-11-19 19:18:18 -0500

    bootable

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -4,15 +4,32 @@ window.App = new Backbone.Mobile.Application({
   Routers: {},
   Views: {},
 });
-  
+
 App.addRegions({
   appRegion: '#main'
 });
 
-App.addInitializer = function(){
-  new Backbone.Router()
-}
+// =======================================
+// demo app codebase here
+// =======================================
+
+App.Routers.Main = Backbone.Router.extend({
+  routes: {
+    ".*" : "index",
+  },
+  index: function(){
+    console.log('index....')      
+  }
+})
   
+// =======================================
+// initialize the app
+// =======================================
+
+App.addInitializer(function() {
+  new App.Routers.Main()
+});
+
 App.on('initialize:after',function(){ 
   Backbone.history.start()
 });
